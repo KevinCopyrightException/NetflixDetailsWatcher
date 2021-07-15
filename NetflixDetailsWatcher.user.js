5fef7e53a9ed5ae6acd844aeb4efbbfc91292ced
Kevin Greßmann <kevin.gressmann@hagen-triebsch.de>
2021-07-15 22:26:10 +0200

    V

--- NetflixDetailsWatcher.user.js
+++ NetflixDetailsWatcher.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Netflix Details Watcher
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  
 // @author       KG
 // @match        https://www.netflix.com/*
